335608e893d6dc28f5f6bab3942ce16e80be17b5
David Kennedy <david01181998@gmail.com>
2019-08-07 18:45:47 -0400

    Inital commit of current work

---
+++ DemoGame/Input.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Creating Input class with 4 properities for 4 movements
+//Each properity needs to be set with Keys
+
+namespace DemoGame
+{
+    public class Input
+    {
+        public Keys Left { get; set; } // Using properties
+        // same thing as defining public Keys Left variable and
+        // adding get and set function. For right we use example
+
+        public Keys Right { get; set; }
+        public Keys Up { get; set; }
+        public Keys Down { get; set; }
+        
+    }
+}
---
+++ DemoGame/Sprite.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGame
+{
+    public class Sprite
+    {
+        private Texture2D texture;
+        private Vector2 position;
+        public Input Input; 
+        public float speed = 1f;
+
+        public Sprite(Texture2D texture)
+            // Constructor with only Texture
+        {
+            this.texture = texture;
+        }
+
+        public Sprite(Texture2D texture, Vector2 position)
+            // Constructor with Texture and Position
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+
+        private void Move()
+        {
+            if (Input == null)
+            {
+                return;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Input.Left))
+            {
+                position.X -= this.speed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Input.Right))
+            {
+                position.X += this.speed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Input.Up))
+            {
+                position.Y -= this.speed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Input.Down))
+            {
+                position.Y += this.speed;
+            }
+        }
+
+        public void Update()
+        {
+            this.Move();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+            // Passing SpriteBatch object from Game1.cs
+            //SpriteBatch object is used for drawing
+        {
+            spriteBatch.Draw(this.texture, this.position, Color.White);
+        }
+    }
+}

833782e1324c8ed524778dbb04e643c42b9980f0
David kennedy <43424227+Davidkennedy072@users.noreply.github.com>
2019-08-20 10:09:52 -0400

    Added death and take damage to statsmanager.cs

--- DemoGame/Stats/StatsManager.cs
+++ DemoGame/Stats/StatsManager.cs
@@ -10,17 +10,31 @@ namespace DemoGame.Stats
     {
         public float health { get; set; }
         public float speed {get; set; }
+        public bool dead {get; set; }
 
         public StatsManager()
         {
             this.health = 100f;
             this.speed = 1f;
+            this.dead = false;
         }
 
         public StatsManager(float health = 100, float speed = 1f)
         {
             this.health = health;
             this.speed = speed;
+            this.dead = false;
+        }
+        
+        public TakeDamage(float damage)
+        {
+            this.health = this.health - damage;
+            
+            if this.health <= 0
+            {
+                this.health = 0;
+                this.dead = true;
+            }
         }
     }
 }
